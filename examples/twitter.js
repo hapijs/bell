7a40b49509281ff2011d4585962e8c20df4ed2f6
Eran Hammer <eran@hueniverse.com>
2014-06-03 01:56:57 -0700

    Google, facebook, yahoo

--- examples/twitter.js
+++ examples/twitter.js
@@ -5,16 +5,17 @@ var Hoek = require('hoek');
 var Bell = require('../');
 
 
-var server = new Hapi.Server(8000, { location: 'http://localhost:8000' });
+var server = new Hapi.Server(8000, { location: 'http://localhost:8000' });      // Change localhost to your domain
 server.pack.register(Bell, function (err) {
 
     Hoek.assert(!err);
 
     server.auth.strategy('twitter', 'bell', {
+        provider: 'twitter',
         password: 'password',
         isSecure: false,
-        clientId: '',
-        clientSecret: ''
+        clientId: '',                               // Set client id
+        clientSecret: ''                            // Set client secret
     });
 
     server.route({
