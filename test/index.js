118f1c6dc61f9acaf9a16304a3beeb5632976e89
Eran Hammer <eran@hueniverse.com>
2014-06-03 19:05:53 -0700

    Test oauth 1 and 2

--- test/index.js
+++ test/index.js
@@ -1,10 +1,9 @@
 // Load modules
 
-var Querystring = require('querystring');
 var Lab = require('lab');
 var Hapi = require('hapi');
-var Hawk = require('hawk');
 var Bell = require('../');
+var Mock = require('./mock');
 
 
 // Declare internals
@@ -25,7 +24,7 @@ describe('Bell', function () {
 
     it('authenticates an endpoint via oauth', function (done) {
 
-        var mock = new internals.OAuthMock();
+        var mock = new Mock.V1();
         mock.start(function (provider) {
 
             var server = new Hapi.Server('localhost');
@@ -56,13 +55,88 @@ describe('Bell', function () {
                 server.inject('/', function (res) {
 
                     expect(res.headers.location).to.equal('http://localhost:80/bell/door?next=%2F');
-                    server.inject(res.headers.location.replace('http://localhost:80', ''), function (res) {
 
+                    server.inject(res.headers.location, function (res) {
+
+                        var cookie = res.headers['set-cookie'][0].split(';')[0] + ';';
                         expect(res.headers.location).to.equal(mock.uri + '/auth?oauth_token=1');
-                        mock.server.inject('/auth?oauth_token=1', function (res) {
+
+                        mock.server.inject(res.headers.location, function (res) {
 
                             expect(res.headers.location).to.equal('http://localhost:80/bell/door?oauth_token=1&oauth_verifier=123');
-                            mock.stop(done);
+
+                            server.inject({ url: res.headers.location, headers: { cookie: cookie } }, function (res) {
+
+                                var cookie = res.headers['set-cookie'][0].split(';')[0] + ';';
+                                expect(res.headers.location).to.equal('http://localhost:80/');
+
+                                server.inject({ url: res.headers.location, headers: { cookie: cookie } }, function (res) {
+
+                                    expect(res.result.status).to.equal('authenticated');
+                                    mock.stop(done);
+                                });
+                            });
+                        });
+                    });
+                });
+            });
+        });
+    });
+
+    it('authenticates an endpoint via oauth2', function (done) {
+
+        var mock = new Mock.V2();
+        mock.start(function (provider) {
+
+            var server = new Hapi.Server('localhost');
+            server.pack.register(Bell, function (err) {
+
+                expect(err).to.not.exist;
+
+                server.auth.strategy('custom', 'bell', {
+                    password: 'password',
+                    isSecure: false,
+                    clientId: 'test',
+                    clientSecret: 'secret',
+                    provider: provider
+                });
+
+                server.route({
+                    method: 'GET',
+                    path: '/',
+                    config: {
+                        auth: 'custom',
+                        handler: function (request, reply) {
+
+                            reply(request.auth.credentials);
+                        }
+                    }
+                });
+
+                server.inject('/', function (res) {
+
+                    expect(res.headers.location).to.equal('http://localhost:80/bell/door?next=%2F');
+
+                    server.inject(res.headers.location, function (res) {
+
+                        var cookie = res.headers['set-cookie'][0].split(';')[0] + ';';
+                        expect(res.headers.location).to.contain(mock.uri + '/auth?client_id=test&response_type=code&redirect_uri=http%3A%2F%2Flocalhost%3A80%2Fbell%2Fdoor&state=');
+
+                        mock.server.inject(res.headers.location, function (res) {
+
+                            expect(res.headers.location).to.contain('http://localhost:80/bell/door?code=1&state=');
+
+                            server.inject({ url: res.headers.location, headers: { cookie: cookie } }, function (res) {
+
+                                var cookie = res.headers['set-cookie'][0].split(';')[0] + ';';
+                                expect(res.headers.location).to.equal('http://localhost:80/');
+
+                                server.inject({ url: res.headers.location, headers: { cookie: cookie } }, function (res) {
+
+                                    expect(res.result.status).to.equal('authenticated');
+                                    mock.stop(done);
+                                });
+                            });
                         });
                     });
                 });
@@ -70,106 +144,3 @@ describe('Bell', function () {
         });
     });
 });
-
-
-internals.OAuthMock = function () {
-
-    this.tokens = {};
-
-    this.server = new Hapi.Server(0, 'localhost');
-    this.server.route([
-        {
-            method: 'POST',
-            path: '/temporary',
-            config: {
-                bind: this,
-                handler: function (request, reply) {
-
-                    var header = Hawk.utils.parseAuthorizationHeader(request.headers.authorization.replace(/OAuth/i, 'Hawk'), ['realm', 'oauth_consumer_key', 'oauth_signature_method', 'oauth_callback', 'oauth_signature', 'oauth_version', 'oauth_timestamp', 'oauth_nonce']);
-                    expect(header.oauth_callback).to.exist;
-
-                    var token = String(Object.keys(this.tokens).length + 1);
-                    this.tokens[token] = {
-                        authorized: false,
-                        secret: 'secret',
-                        callback: header.oauth_callback
-                    };
-
-                    var payload = {
-                        oauth_token: token,
-                        oauth_token_secret: 'secret',
-                        oauth_callback_confirmed: true
-                    };
-
-                    reply(Querystring.encode(payload)).type('application/x-www-form-urlencoded');
-                }
-            }
-        },
-        {
-            method: 'GET',
-            path: '/auth',
-            config: {
-                bind: this,
-                handler: function (request, reply) {
-
-                    var token = this.tokens[request.query.oauth_token];
-                    expect(token).to.exist;
-
-                    token.authorized = true;
-                    token.verifier = '123';
-                    
-                    reply().redirect(unescape(token.callback) + '?oauth_token=' + request.query.oauth_token + '&oauth_verifier=' + token.verifier);
-                }
-            }
-        },
-        {
-            method: 'POST',
-            path: '/token',
-            config: {
-                bind: this,
-                handler: function (request, reply) {
-
-                    var header = Hawk.utils.parseAuthorizationHeader(request.headers.authorization.replace(/OAuth/i, 'Hawk'), ['realm', 'oauth_consumer_key', 'oauth_token', 'oauth_signature_method', 'oauth_verifier', 'oauth_signature', 'oauth_version', 'oauth_timestamp', 'oauth_nonce']);
-
-                    var token = this.tokens[request.query.oauth_token];
-                    expect(token).to.exist;
-                    expect(token.verifier).to.equal(header.oauth_verifier);
-                    expect(token.authorized).to.equal(true);
-
-                    var payload = {
-                        oauth_token: 'final',
-                        oauth_token_secret: 'secret'
-                    };
-
-                    reply(Querystring.encode(payload)).type('application/x-www-form-urlencoded');
-                }
-            }
-        }
-    ]);
-};
-
-
-internals.OAuthMock.prototype.start = function (callback) {
-
-    var self = this;
-
-    this.server.start(function (err) {
-
-        expect(err).to.not.exist;
-
-        self.uri = self.server.info.uri;
-
-        return callback({
-            protocol: 'oauth',
-            temporary: self.server.info.uri + '/temporary',
-            auth: self.server.info.uri + '/auth',
-            token: self.server.info.uri + '/token'
-        });
-    })
-};
-
-
-internals.OAuthMock.prototype.stop = function (callback) {
-
-    this.server.stop(callback);
-};
---
+++ test/mock.js
@@ -0,0 +1,194 @@
+// Load modules
+
+var Querystring = require('querystring');
+var Hawk = require('hawk');
+var Lab = require('lab');
+var Hapi = require('hapi');
+
+
+// Declare internals
+
+var internals = {};
+
+
+// Test shortcuts
+
+var expect = Lab.expect;
+var before = Lab.before;
+var after = Lab.after;
+var describe = Lab.experiment;
+
+
+exports.V1 = internals.V1 = function () {
+
+    this.tokens = {};
+
+    this.server = new Hapi.Server(0, 'localhost');
+    this.server.route([
+        {
+            method: 'POST',
+            path: '/temporary',
+            config: {
+                bind: this,
+                handler: function (request, reply) {
+
+                    var header = Hawk.utils.parseAuthorizationHeader(request.headers.authorization.replace(/OAuth/i, 'Hawk'), ['realm', 'oauth_consumer_key', 'oauth_signature_method', 'oauth_callback', 'oauth_signature', 'oauth_version', 'oauth_timestamp', 'oauth_nonce']);
+                    expect(header.oauth_callback).to.exist;
+
+                    var token = String(Object.keys(this.tokens).length + 1);
+                    this.tokens[token] = {
+                        authorized: false,
+                        secret: 'secret',
+                        callback: header.oauth_callback
+                    };
+
+                    var payload = {
+                        oauth_token: token,
+                        oauth_token_secret: 'secret',
+                        oauth_callback_confirmed: true
+                    };
+
+                    reply(Querystring.encode(payload)).type('application/x-www-form-urlencoded');
+                }
+            }
+        },
+        {
+            method: 'GET',
+            path: '/auth',
+            config: {
+                bind: this,
+                handler: function (request, reply) {
+
+                    var token = this.tokens[request.query.oauth_token];
+                    expect(token).to.exist;
+
+                    token.authorized = true;
+                    token.verifier = '123';
+
+                    reply().redirect(unescape(token.callback) + '?oauth_token=' + request.query.oauth_token + '&oauth_verifier=' + token.verifier);
+                }
+            }
+        },
+        {
+            method: 'POST',
+            path: '/token',
+            config: {
+                bind: this,
+                handler: function (request, reply) {
+
+                    var header = Hawk.utils.parseAuthorizationHeader(request.headers.authorization.replace(/OAuth/i, 'Hawk'), ['realm', 'oauth_consumer_key', 'oauth_token', 'oauth_signature_method', 'oauth_verifier', 'oauth_signature', 'oauth_version', 'oauth_timestamp', 'oauth_nonce']);
+                    var token = this.tokens[header.oauth_token];
+                    expect(token).to.exist;
+                    expect(token.verifier).to.equal(header.oauth_verifier);
+                    expect(token.authorized).to.equal(true);
+
+                    var payload = {
+                        oauth_token: 'final',
+                        oauth_token_secret: 'secret'
+                    };
+
+                    reply(Querystring.encode(payload)).type('application/x-www-form-urlencoded');
+                }
+            }
+        }
+    ]);
+};
+
+
+internals.V1.prototype.start = function (callback) {
+
+    var self = this;
+
+    this.server.start(function (err) {
+
+        expect(err).to.not.exist;
+
+        self.uri = self.server.info.uri;
+
+        return callback({
+            protocol: 'oauth',
+            temporary: self.server.info.uri + '/temporary',
+            auth: self.server.info.uri + '/auth',
+            token: self.server.info.uri + '/token'
+        });
+    })
+};
+
+
+internals.V1.prototype.stop = function (callback) {
+
+    this.server.stop(callback);
+};
+
+
+exports.V2 = internals.V2 = function () {
+
+    this.codes = {};
+
+    this.server = new Hapi.Server(0, 'localhost');
+    this.server.route([
+        {
+            method: 'GET',
+            path: '/auth',
+            config: {
+                bind: this,
+                handler: function (request, reply) {
+
+                    var code = String(Object.keys(this.codes).length + 1);
+                    this.codes[code] = {
+                        redirect_uri: request.query.redirect_uri,
+                        client_id: request.query.client_id
+                    };
+
+                    reply().redirect(request.query.redirect_uri + '?code=' + code + '&state=' + request.query.state);
+                }
+            }
+        },
+        {
+            method: 'POST',
+            path: '/token',
+            config: {
+                bind: this,
+                handler: function (request, reply) {
+
+                    var code = this.codes[request.payload.code];
+                    expect(code).to.exist;
+                    expect(code.redirect_uri).to.equal(request.payload.redirect_uri);
+                    expect(code.client_id).to.equal(request.payload.client_id);
+
+                    var payload = {
+                        access_token: '456',
+                        expires_in: 3600
+                    };
+
+                    reply(payload);
+                }
+            }
+        }
+    ]);
+};
+
+
+internals.V2.prototype.start = function (callback) {
+
+    var self = this;
+
+    this.server.start(function (err) {
+
+        expect(err).to.not.exist;
+
+        self.uri = self.server.info.uri;
+
+        return callback({
+            protocol: 'oauth2',
+            auth: self.server.info.uri + '/auth',
+            token: self.server.info.uri + '/token'
+        });
+    })
+};
+
+
+internals.V2.prototype.stop = function (callback) {
+
+    this.server.stop(callback);
+};
