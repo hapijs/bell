39e421219bf5f6bcae28ba4f479f83928edc4fd0
Eran Hammer <eran@hueniverse.com>
2014-06-05 19:54:51 -0700

    misc

--- test/twitter.js
+++ test/twitter.js
@@ -4,6 +4,7 @@ var Lab = require('lab');
 var Hapi = require('hapi');
 var Hoek = require('hoek');
 var Bell = require('../');
+var Providers = require('../lib/providers');
 var Mock = require('./mock');
 
 
@@ -23,7 +24,7 @@ var it = Lab.test;
 
 describe('Bell', function () {
 
-    it('authenticates an endpoint via oauth', function (done) {
+    it('authenticates with mock Twitter', { parallel: false }, function (done) {
 
         var mock = new Mock.V1();
         mock.start(function (provider) {
@@ -33,12 +34,19 @@ describe('Bell', function () {
 
                 expect(err).to.not.exist;
 
+                var origProvider = Hoek.clone(Providers.twitter);
+                Hoek.merge(Providers.twitter, provider);
+
+                Mock.override('https://api.twitter.com/1.1/users/show.json', {
+                    property: 'something'
+                });
+
                 server.auth.strategy('custom', 'bell', {
                     password: 'password',
                     isSecure: false,
-                    clientId: 'test',
+                    clientId: 'twitter',
                     clientSecret: 'secret',
-                    provider: provider
+                    provider: 'twitter'
                 });
 
                 server.route({
@@ -73,7 +81,22 @@ describe('Bell', function () {
 
                                 server.inject({ url: res.headers.location, headers: { cookie: cookie } }, function (res) {
 
-                                    expect(res.result.status).to.equal('authenticated');
+                                    expect(res.result).to.deep.equal({
+                                        provider: 'twitter',
+                                        status: 'authenticated',
+                                        token: 'final',
+                                        secret: 'secret',
+                                        profile: {
+                                            id: '1234567890',
+                                            username: 'Steve Stevens',
+                                            raw: {
+                                                property: 'something'
+                                            }
+                                        }
+                                    });
+
+                                    Providers.twitter = origProvider;
+                                    Mock.clear();
                                     mock.stop(done);
                                 });
                             });
@@ -84,9 +107,9 @@ describe('Bell', function () {
         });
     });
 
-    it('authenticates an endpoint via oauth2', function (done) {
+    it('authenticates with mock Twitter (without extended profile)', { parallel: false }, function (done) {
 
-        var mock = new Mock.V2();
+        var mock = new Mock.V1();
         mock.start(function (provider) {
 
             var server = new Hapi.Server('localhost');
@@ -94,12 +117,16 @@ describe('Bell', function () {
 
                 expect(err).to.not.exist;
 
+                var origProvider = Hoek.clone(Providers.twitter);
+                Hoek.merge(Providers.twitter, provider);
+
                 server.auth.strategy('custom', 'bell', {
                     password: 'password',
                     isSecure: false,
-                    clientId: 'test',
+                    clientId: 'twitter',
                     clientSecret: 'secret',
-                    provider: provider
+                    provider: 'twitter',
+                    extendedProfile: false
                 });
 
                 server.route({
@@ -121,11 +148,11 @@ describe('Bell', function () {
                     server.inject(res.headers.location, function (res) {
 
                         var cookie = res.headers['set-cookie'][0].split(';')[0] + ';';
-                        expect(res.headers.location).to.contain(mock.uri + '/auth?client_id=test&response_type=code&redirect_uri=http%3A%2F%2Flocalhost%3A80%2Fbell%2Fdoor&state=');
+                        expect(res.headers.location).to.equal(mock.uri + '/auth?oauth_token=1');
 
                         mock.server.inject(res.headers.location, function (res) {
 
-                            expect(res.headers.location).to.contain('http://localhost:80/bell/door?code=1&state=');
+                            expect(res.headers.location).to.equal('http://localhost:80/bell/door?oauth_token=1&oauth_verifier=123');
 
                             server.inject({ url: res.headers.location, headers: { cookie: cookie } }, function (res) {
 
@@ -134,7 +161,18 @@ describe('Bell', function () {
 
                                 server.inject({ url: res.headers.location, headers: { cookie: cookie } }, function (res) {
 
-                                    expect(res.result.status).to.equal('authenticated');
+                                    expect(res.result).to.deep.equal({
+                                        provider: 'twitter',
+                                        status: 'authenticated',
+                                        token: 'final',
+                                        secret: 'secret',
+                                        profile: {
+                                            id: '1234567890',
+                                            username: 'Steve Stevens'
+                                        }
+                                    });
+
+                                    Providers.twitter = origProvider;
                                     mock.stop(done);
                                 });
                             });
@@ -145,7 +183,7 @@ describe('Bell', function () {
         });
     });
 
-    it('overrides cookie name', function (done) {
+    it('authenticates with mock Twitter (profile error)', { parallel: false }, function (done) {
 
         var mock = new Mock.V1();
         mock.start(function (provider) {
@@ -155,13 +193,15 @@ describe('Bell', function () {
 
                 expect(err).to.not.exist;
 
+                var origProvider = Hoek.clone(Providers.twitter);
+                Hoek.merge(Providers.twitter, provider);
+
                 server.auth.strategy('custom', 'bell', {
                     password: 'password',
                     isSecure: false,
-                    clientId: 'test',
+                    clientId: 'twitter',
                     clientSecret: 'secret',
-                    provider: provider,
-                    cookie: 'ring'
+                    provider: 'twitter'
                 });
 
                 server.route({
@@ -182,49 +222,21 @@ describe('Bell', function () {
 
                     server.inject(res.headers.location, function (res) {
 
-                        expect(res.headers['set-cookie'][0]).to.contain('ring=');
-                        mock.stop(done);
+                        var cookie = res.headers['set-cookie'][0].split(';')[0] + ';';
+                        expect(res.headers.location).to.equal(mock.uri + '/auth?oauth_token=1');
+
+                        mock.server.inject(res.headers.location, function (res) {
+
+                            expect(res.headers.location).to.equal('http://localhost:80/bell/door?oauth_token=1&oauth_verifier=123');
+
+                            server.inject({ url: res.headers.location, headers: { cookie: cookie } }, function (res) {
+
+                                expect(res.statusCode).to.equal(500);
+                                Providers.twitter = origProvider;
+                                mock.stop(done);
+                            });
+                        });
                     });
-                });
-            });
-        });
-    });
-
-    it('overrides internal endpoint path', function (done) {
-
-        var mock = new Mock.V1();
-        mock.start(function (provider) {
-
-            var server = new Hapi.Server('localhost');
-            server.pack.register(Bell, function (err) {
-
-                expect(err).to.not.exist;
-
-                server.auth.strategy('custom', 'bell', {
-                    password: 'password',
-                    isSecure: false,
-                    clientId: 'test',
-                    clientSecret: 'secret',
-                    provider: provider,
-                    path: '/somewhere/else'
-                });
-
-                server.route({
-                    method: 'GET',
-                    path: '/',
-                    config: {
-                        auth: 'custom',
-                        handler: function (request, reply) {
-
-                            reply(request.auth.credentials);
-                        }
-                    }
-                });
-
-                server.inject('/', function (res) {
-
-                    expect(res.headers.location).to.equal('http://localhost:80/somewhere/else?next=%2F');
-                    mock.stop(done);
                 });
             });
         });
