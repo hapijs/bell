9d04d2cca12f0ac8f8e6acdcf3f0d4e48c5ae9fa
Dave Stevens <dstevens@zappos.com>
2014-07-11 23:23:49 -0700

    Add Foursquare as built-in provider

--- test/providers.js
+++ test/providers.js
@@ -859,4 +859,184 @@ describe('Bell', function () {
             });
         });
     });
+
+    describe('#foursquare', function () {
+
+        it('authenticates with mock', { parallel: false }, function (done) {
+
+            var mock = new Mock.V1();
+            mock.start(function (provider) {
+
+                var server = new Hapi.Server('localhost');
+                server.pack.register(Bell, function (err) {
+
+                    expect(err).to.not.exist;
+
+                    var custom = Bell.providers.foursquare();
+                    Hoek.merge(custom, provider);
+
+                    var data = {
+                        response: {
+                            user: {
+                                id: "1234",
+                                firstName: "Steve",
+                                lastName: "Smith",
+                                gender: "male",
+                                relationship: "self",
+                                photo: {
+                                    prefix: "https://irs0.4sqi.net/img/user/",
+                                    suffix: "/1234-K0KG0PLWAG1WTOXM.jpg"
+                                },
+                                contact: {
+                                    email: "stevesmith@test.com"
+                                }
+                            }
+                        }
+                    };
+
+                    Mock.override('https://api.foursquare.com/v2/users/self', data);
+
+                    server.auth.strategy('custom', 'bell', {
+                        password: 'password',
+                        isSecure: false,
+                        clientId: 'foursquare',
+                        clientSecret: 'secret',
+                        provider: custom
+                    });
+
+                    server.route({
+                        method: '*',
+                        path: '/login',
+                        config: {
+                            auth: 'custom',
+                            handler: function (request, reply) {
+
+                                reply(request.auth.credentials);
+                            }
+                        }
+                    });
+
+                    server.inject('/login', function (res) {
+
+                        var cookie = res.headers['set-cookie'][0].split(';')[0] + ';';
+                        mock.server.inject(res.headers.location, function (res) {
+
+                            server.inject({ url: res.headers.location, headers: { cookie: cookie } }, function (res) {
+
+                                expect(res.result).to.deep.equal({
+                                    provider: 'custom',
+                                    token: 'final',
+                                    secret: 'secret',
+                                    query: {},
+                                    profile: {
+
+                                        id: '1234',
+                                        displayName: 'Steve Smith',
+                                        name: {
+                                            first: 'Steve',
+                                            last: 'Smith'
+                                        },
+                                        email: data.response.user.contact.email,
+                                        raw: data.response.user
+                                    }
+                                });
+
+                                Mock.clear();
+                                mock.stop(done);
+                            });
+                        });
+                    });
+                });
+            });
+        });
+
+        it('authenticates with mock when user has no email set', { parallel: false }, function (done) {
+
+            var mock = new Mock.V1();
+            mock.start(function (provider) {
+
+                var server = new Hapi.Server('localhost');
+                server.pack.register(Bell, function (err) {
+
+                    expect(err).to.not.exist;
+
+                    var custom = Bell.providers.foursquare();
+                    Hoek.merge(custom, provider);
+
+                    var data = {
+                        response: {
+                            user: {
+                                id: "1234",
+                                firstName: "Steve",
+                                lastName: "Smith",
+                                gender: "male",
+                                relationship: "self",
+                                photo: {
+                                    prefix: "https://irs0.4sqi.net/img/user/",
+                                    suffix: "/1234-K0KG0PLWAG1WTOXM.jpg"
+                                },
+                                contact: {
+                                    facebook: "http://facebook.com/stevesmith.test"
+                                }
+                            }
+                        }
+                    };
+
+                    Mock.override('https://api.foursquare.com/v2/users/self', data);
+
+                    server.auth.strategy('custom', 'bell', {
+                        password: 'password',
+                        isSecure: false,
+                        clientId: 'foursquare',
+                        clientSecret: 'secret',
+                        provider: custom
+                    });
+
+                    server.route({
+                        method: '*',
+                        path: '/login',
+                        config: {
+                            auth: 'custom',
+                            handler: function (request, reply) {
+
+                                reply(request.auth.credentials);
+
+                            }
+                        }
+                    });
+
+                    server.inject('/login', function (res) {
+
+                        var cookie = res.headers['set-cookie'][0].split(';')[0] + ';';
+                        mock.server.inject(res.headers.location, function (res) {
+
+                            server.inject({ url: res.headers.location, headers: { cookie: cookie } }, function (res) {
+
+                                expect(res.result).to.deep.equal({
+                                    provider: 'custom',
+                                    token: 'final',
+                                    secret: 'secret',
+                                    query: {},
+                                    profile: {
+
+                                        id: '1234',
+                                        displayName: 'Steve Smith',
+                                        name: {
+                                            first: 'Steve',
+                                            last: 'Smith'
+                                        },
+                                        email: undefined,
+                                        raw: data.response.user
+                                    }
+                                });
+
+                                Mock.clear();
+                                mock.stop(done);
+                            });
+                        });
+                    });
+                });
+            });
+        });
+    });
 });
