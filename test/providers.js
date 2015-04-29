7b5466759713da87388a4a08ed00012e25c2b6a8
Charles Lawrence <charleslawrence@charles-macbook.local>
2015-04-28 23:35:04 -0700

    add reddit as oauth provider and enable basic http authentication header as required for reddit oauth

--- test/providers.js
+++ test/providers.js
@@ -23,6 +23,85 @@ var expect = Code.expect;
 
 describe('Bell', function () {
 
+    describe('#reddit', function () {
+
+        it('authenticates with mock', { parallel: false }, function (done) {
+
+            var mock = new Mock.V2();
+            mock.start(function (provider) {
+
+                var server = new Hapi.Server();
+                server.connection({ host: 'localhost', port: 80 });
+                server.register(Bell, function (err) {
+
+                    expect(err).to.not.exist();
+
+                    var custom = Bell.providers.reddit();
+                    Hoek.merge(custom, provider);
+
+                    var profile = {
+                        name: 'john',
+                        created: 0,
+                        created_utc: 0,
+                        hide_from_robots: true,
+                        gold_creddits: 0,
+                        link_karma: 0,
+                        comment_karma: 0,
+                        over_18: true,
+                        is_gold: false,
+                        is_mod: true,
+                        gold_expiration: null,
+                        has_verified_email: true,
+                        id: 'abcde',
+                        inbox_count: 0
+                    };
+
+                    Mock.override('https://oauth.reddit.com/api/v1/me', profile);
+
+                    server.auth.strategy('custom', 'bell', {
+                        password: 'password',
+                        isSecure: false,
+                        clientId: 'reddit',
+                        clientSecret: 'secret',
+                        provider: custom
+                    });
+
+                    server.route({
+                        method: '*',
+                        path: '/login',
+                        config: {
+                            auth: 'custom',
+                            handler: function (request, reply) {
+                                reply(request.auth.credentials);
+                            }
+                        }
+                    });
+
+                    server.inject('/login', function (res) {
+
+                        var cookie = res.headers['set-cookie'][0].split(';')[0] + ';';
+                        mock.server.inject(res.headers.location, function (res) {
+
+                            server.inject({ url: res.headers.location, headers: { cookie: cookie } }, function (res) {
+                                expect(res.result).to.deep.equal({
+                                    provider: 'custom',
+                                    token: '456',
+                                    expiresIn: 3600,
+                                    refreshToken: undefined,
+                                    query: {},
+                                    profile: profile
+                                });
+
+                                Mock.clear();
+                                mock.stop(done);
+                            });
+                        });
+                    });
+                });
+            });
+        });
+    });
+
     describe('#facebook', function () {
 
         it('authenticates with mock', { parallel: false }, function (done) {
