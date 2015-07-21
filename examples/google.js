2d840ebfc6a3a1dcc74a4fefbe10878524416a21
Drew Whitehouse <Drew.Whitehouse@anu.edu.au>
2015-07-21 11:54:29 +1000

    Update google.js
    Added a comment to the google.js example describing the enabling of the Google+ API for the project.

--- examples/google.js
+++ examples/google.js
@@ -19,6 +19,8 @@ server.register(Bell, function (err) {
         // Select "web application" and set "AUTHORIZED JAVASCRIPT ORIGINS" and "AUTHORIZED REDIRECT URIS"
         // This will net you the clientId and the clientSecret needed.
         // Also be sure to pass the location as well. It must be in the list of "AUTHORIZED REDIRECT URIS"
+        // You must also enable the Google+ API in your profile.
+        // Go to APIs & Auth, then APIs and under Social APIs click Google+ API and enable it.
         clientId: '',
         clientSecret: '',
         location: server.info.uri
