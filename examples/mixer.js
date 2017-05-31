c9c19ab75359d3577a1e459f4c0b3d2914cadfd1
jerome724 <dev@jerome724.com>
2017-05-31 22:43:39 +0200

    fixed travis-ci error reports

--- examples/mixer.js
+++ examples/mixer.js
@@ -6,42 +6,48 @@ const Hapi = require('hapi');
 const Hoek = require('hoek');
 const Bell = require('../');
 
+
 const server = new Hapi.Server();
 server.connection({
-    host: 'localhost',
-    port: 4567
+    port: 8000
 });
 
 server.register(Bell, (err) => {
+
     Hoek.assert(!err, err);
     server.auth.strategy('mixer', 'bell', {
         provider: 'mixer',
         password: 'cookie_encryption_password_secure',
         isSecure: false,
         /**
-         * you have to register your OAuth client here: https://mixer.com/lab/oauth
+         * you need to register your OAuth client here: https://mixer.com/lab/oauth
          * for all scopes see https://dev.mixer.com/reference/oauth/index.html#oauth_scopes
          */
         clientId: '',
         clientSecret: ''
+        // Uncomment the below line for more scopes (check Twitch API documentation), "user:details:self" scope is set as default
+        // scope: ['user:details:self']
     });
 
     server.route({
-        method: '*',
+        method: ['GET', 'POST'],
         path: '/bell/door',
         config: {
             auth: 'mixer',
             handler: function (request, reply) {
+
                 if (!request.auth.isAuthenticated) {
                     return reply('Authentication failed due to: ' + request.auth.error.message);
                 }
+
                 reply('<pre>' + JSON.stringify(request.auth.credentials, null, 4) + '</pre>');
+
             }
         }
     });
 
     server.start((err) => {
-        
+
         Hoek.assert(!err, err);
         console.log('Server started at:', server.info.uri);
     })
