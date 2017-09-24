3a4a7c268f9d25caf674ea34b719c538f3fc5102
Phi Tran <ptran@salesforce.com>
2017-09-24 11:19:52 -0700

    Update to Salesforce Provider (#313)
    * update salesforce provider to support extendedProfile config
    support both oauth/userInfo and salesforce identity service profile endpoints
    
    * update documentation
    
    * bit of cleanup

--- test/providers/salesforce.js
+++ test/providers/salesforce.js
@@ -20,6 +20,29 @@ const expect = Code.expect;
 
 describe('salesforce', () => {
 
+    it('fails with extendedProfile false and identityServiceProfile true', { parallel: false }, (done) => {
+
+        const mock = new Mock.V2();
+        mock.start((provider) => {
+
+            const server = new Hapi.Server();
+            server.connection({ host: 'localhost', port: 80 });
+            server.register(Bell, (err) => {
+
+                expect(err).to.not.exist();
+
+                const throws = () => {
+
+                    Bell.providers.salesforce({ extendedProfile: false, identityServiceProfile: true });
+                };
+
+                expect(throws).to.throw(Error);
+
+                mock.stop(done);
+            });
+        });
+    });
+
     it('authenticates with mock', { parallel: false }, (done) => {
 
         const mock = new Mock.V2();
@@ -32,6 +55,79 @@ describe('salesforce', () => {
                 expect(err).to.not.exist();
 
                 const custom = Bell.providers.salesforce();
+
+                expect(custom.auth).to.equal('https://login.salesforce.com/services/oauth2/authorize');
+                expect(custom.token).to.equal('https://login.salesforce.com/services/oauth2/token');
+
+                Hoek.merge(custom, provider);
+
+                const profile = {
+                    user_id: '1234567890',
+                    username: 'steve',
+                    display_name: 'steve',
+                    first_name: 'steve',
+                    last_name: 'smith',
+                    email: 'steve@example.com'
+                };
+
+                Mock.override('https://login.salesforce.com/services/oauth2/userinfo', profile);
+
+                server.auth.strategy('custom', 'bell', {
+                    password: 'cookie_encryption_password_secure',
+                    isSecure: false,
+                    clientId: 'salesforce',
+                    clientSecret: 'secret',
+                    provider: custom
+                });
+
+                server.route({
+                    method: '*',
+                    path: '/login',
+                    config: {
+                        auth: 'custom',
+                        handler: function (request, reply) {
+
+                            reply(request.auth.credentials);
+                        }
+                    }
+                });
+
+                server.inject('/login', (res) => {
+
+                    const cookie = res.headers['set-cookie'][0].split(';')[0] + ';';
+                    mock.server.inject(res.headers.location, (mockRes) => {
+
+                        server.inject({ url: mockRes.headers.location, headers: { cookie } }, (response) => {
+
+                            Mock.clear();
+                            expect(response.result).to.equal({
+                                provider: 'custom',
+                                token: '456',
+                                expiresIn: 3600,
+                                refreshToken: undefined,
+                                query: {},
+                                profile
+                            });
+                            mock.stop(done);
+                        });
+                    });
+                });
+            });
+        });
+    });
+
+    it('authenticates with mock and custom uri', { parallel: false }, (done) => {
+
+        const mock = new Mock.V2();
+        mock.start((provider) => {
+
+            const server = new Hapi.Server();
+            server.connection({ host: 'localhost', port: 80 });
+            server.register(Bell, (err) => {
+
+                expect(err).to.not.exist();
+
+                const custom = Bell.providers.salesforce({ identityServiceProfile: true });
 
                 expect(custom.auth).to.equal('https://login.salesforce.com/services/oauth2/authorize');
                 expect(custom.token).to.equal('https://login.salesforce.com/services/oauth2/token');
@@ -83,15 +179,7 @@ describe('salesforce', () => {
                                 expiresIn: 3600,
                                 refreshToken: undefined,
                                 query: {},
-                                profile: {
-                                    id: '1234567890',
-                                    username: 'steve',
-                                    displayName: 'steve',
-                                    firstName: 'steve',
-                                    lastName: 'smith',
-                                    email: 'steve@example.com',
-                                    raw: profile
-                                }
+                                profile
                             });
                             mock.stop(done);
                         });
@@ -101,7 +189,7 @@ describe('salesforce', () => {
         });
     });
 
-    it('authenticates with mock and custom uri', { parallel: false }, (done) => {
+    it('authenticates with mock and identityServiceProfile', { parallel: false }, (done) => {
 
         const mock = new Mock.V2();
         mock.start((provider) => {
@@ -128,7 +216,7 @@ describe('salesforce', () => {
                     email: 'steve@example.com'
                 };
 
-                Mock.override('https://login.salesforce.com/id/foo/bar', profile);
+                Mock.override('http://example.com/services/oauth2/userinfo', profile);
 
                 server.auth.strategy('custom', 'bell', {
                     password: 'cookie_encryption_password_secure',
@@ -164,15 +252,79 @@ describe('salesforce', () => {
                                 expiresIn: 3600,
                                 refreshToken: undefined,
                                 query: {},
-                                profile: {
-                                    id: '1234567890',
-                                    username: 'steve',
-                                    displayName: 'steve',
-                                    firstName: 'steve',
-                                    lastName: 'smith',
-                                    email: 'steve@example.com',
-                                    raw: profile
-                                }
+                                profile
+                            });
+                            mock.stop(done);
+                        });
+                    });
+                });
+            });
+        });
+    });
+
+    it('authenticates with mock (without extended profile)', { parallel: false }, (done) => {
+
+        const mock = new Mock.V2();
+        mock.start((provider) => {
+
+            const server = new Hapi.Server();
+            server.connection({ host: 'localhost', port: 80 });
+            server.register(Bell, (err) => {
+
+                expect(err).to.not.exist();
+
+                const custom = Bell.providers.salesforce({ extendedProfile: false });
+
+                expect(custom.auth).to.equal('https://login.salesforce.com/services/oauth2/authorize');
+                expect(custom.token).to.equal('https://login.salesforce.com/services/oauth2/token');
+
+                Hoek.merge(custom, provider);
+
+                const profile = {
+                    user_id: '1234567890',
+                    username: 'steve',
+                    display_name: 'steve',
+                    first_name: 'steve',
+                    last_name: 'smith',
+                    email: 'steve@example.com'
+                };
+
+                Mock.override('https://login.salesforce.com/services/oauth2/userinfo', profile);
+
+                server.auth.strategy('custom', 'bell', {
+                    password: 'cookie_encryption_password_secure',
+                    isSecure: false,
+                    clientId: 'salesforce',
+                    clientSecret: 'secret',
+                    provider: custom
+                });
+
+                server.route({
+                    method: '*',
+                    path: '/login',
+                    config: {
+                        auth: 'custom',
+                        handler: function (request, reply) {
+
+                            reply(request.auth.credentials);
+                        }
+                    }
+                });
+
+                server.inject('/login', (res) => {
+
+                    const cookie = res.headers['set-cookie'][0].split(';')[0] + ';';
+                    mock.server.inject(res.headers.location, (mockRes) => {
+
+                        server.inject({ url: mockRes.headers.location, headers: { cookie } }, (response) => {
+
+                            Mock.clear();
+                            expect(response.result).to.equal({
+                                provider: 'custom',
+                                token: '456',
+                                expiresIn: 3600,
+                                refreshToken: undefined,
+                                query: {}
                             });
                             mock.stop(done);
                         });
