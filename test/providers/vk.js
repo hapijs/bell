d12c3d5896b25f19f7dde2272ca48b14eec6bb05
Slaawwa <slaawwa@gmail.com>
2018-07-25 11:45:43 +0300

    Back old test (for travis)

--- test/providers/vk.js
+++ test/providers/vk.js
@@ -93,6 +93,69 @@ describe('vk', () => {
         const server = Hapi.server({ host: 'localhost', port: 80 });
         await server.register(Bell);
 
+        const custom = Bell.providers.vk({ version: '5.6' });
+        Hoek.merge(custom, mock.provider);
+
+        const data = {
+            response: [{
+                id: '1234567890',
+                first_name: 'steve',
+                last_name: 'smith'
+            }]
+        };
+
+        Mock.override('https://api.vk.com/method/users.get', data);
+
+        server.auth.strategy('custom', 'bell', {
+            password: 'cookie_encryption_password_secure',
+            isSecure: false,
+            clientId: 'vk',
+            clientSecret: 'secret',
+            provider: custom
+        });
+
+        server.route({
+            method: '*',
+            path: '/login',
+            config: {
+                auth: 'custom',
+                handler: function (request, h) {
+
+                    return request.auth.credentials;
+                }
+            }
+        });
+
+        const res1 = await server.inject('/login');
+        const cookie = res1.headers['set-cookie'][0].split(';')[0] + ';';
+
+        const res2 = await mock.server.inject(res1.headers.location);
+
+        const res3 = await server.inject({ url: res2.headers.location, headers: { cookie } });
+        expect(res3.result).to.equal({
+            provider: 'custom',
+            token: '456',
+            expiresIn: 3600,
+            refreshToken: undefined,
+            query: {},
+            profile: {
+                id: '1234567890',
+                displayName: 'steve smith',
+                name: {
+                    first: 'steve',
+                    last: 'smith'
+                },
+                raw: data.response[0]
+            }
+        });
+    });
+
+    it('authenticates with mock and custom version low 5', async (flags) => {
+
+        const mock = await Mock.v2(flags);
+        const server = Hapi.server({ host: 'localhost', port: 80 });
+        await server.register(Bell);
+
         const custom = Bell.providers.vk({ version: '4.104' });
         Hoek.merge(custom, mock.provider);
 
