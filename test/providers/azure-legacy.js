11af56210e765c7e02ab360f9577cd15b066c3f6
Eran Hammer <eran@hammer.io>
2019-09-19 22:15:19 -0700

    Restore azuread as azure-legacy. Closes #432

---
+++ test/providers/azure-legacy.js
@@ -0,0 +1,194 @@
+'use strict';
+
+const Bell = require('../..');
+const Code = require('@hapi/code');
+const Hapi = require('@hapi/hapi');
+const Hoek = require('@hapi/hoek');
+const Lab = require('@hapi/lab');
+
+const Mock = require('../mock');
+
+
+const internals = {};
+
+
+const { describe, it } = exports.lab = Lab.script();
+const expect = Code.expect;
+
+
+describe('azure-legacy', () => {
+
+    it('authenticates with mock Azure AD', async (flags) => {
+
+        const profile = {
+            oid: '1234567890',
+            name: 'Sample AD User',
+            upn: 'sample@microsoft.com'
+        };
+
+        const mock = await Mock.v2(flags);
+        const server = Hapi.server({ host: 'localhost', port: 80 });
+        await server.register(Bell);
+
+        const custom = Bell.providers['azure-legacy']();
+        Hoek.merge(custom, mock.provider);
+
+        Mock.override('https://login.microsoftonline.com/common/openid/userinfo', profile);
+
+        server.auth.strategy('custom', 'bell', {
+            password: 'cookie_encryption_password_secure',
+            isSecure: false,
+            clientId: 'azuread',
+            clientSecret: 'secret',
+            provider: custom
+        });
+
+        server.route({
+            method: '*',
+            path: '/login',
+            config: {
+                auth: 'custom',
+                handler: function (request, h) {
+
+                    return request.auth.credentials;
+                }
+            }
+        });
+
+        const res1 = await server.inject('/login');
+        const cookie = res1.headers['set-cookie'][0].split(';')[0] + ';';
+
+        const res2 = await mock.server.inject(res1.headers.location);
+
+        const res3 = await server.inject({ url: res2.headers.location, headers: { cookie } });
+        expect(res3.result).to.equal({
+            provider: 'custom',
+            token: '456',
+            expiresIn: 3600,
+            refreshToken: undefined,
+            query: {},
+            profile: {
+                id: '1234567890',
+                displayName: 'Sample AD User',
+                email: 'sample@microsoft.com',
+                raw: profile
+            }
+        });
+    });
+
+    it('authenticates with mock Azure AD email', async (flags) => {
+
+        const profile = {
+            oid: '1234567890',
+            name: 'Sample AD User',
+            email: 'sample@microsoft.com'
+        };
+
+        const mock = await Mock.v2(flags);
+        const server = Hapi.server({ host: 'localhost', port: 80 });
+        await server.register(Bell);
+
+        const custom = Bell.providers['azure-legacy']();
+        Hoek.merge(custom, mock.provider);
+
+        Mock.override('https://login.microsoftonline.com/common/openid/userinfo', profile);
+
+        server.auth.strategy('custom', 'bell', {
+            password: 'cookie_encryption_password_secure',
+            isSecure: false,
+            clientId: 'azuread',
+            clientSecret: 'secret',
+            provider: custom
+        });
+
+        server.route({
+            method: '*',
+            path: '/login',
+            config: {
+                auth: 'custom',
+                handler: function (request, h) {
+
+                    return request.auth.credentials;
+                }
+            }
+        });
+
+        const res1 = await server.inject('/login');
+        const cookie = res1.headers['set-cookie'][0].split(';')[0] + ';';
+
+        const res2 = await mock.server.inject(res1.headers.location);
+
+        const res3 = await server.inject({ url: res2.headers.location, headers: { cookie } });
+        expect(res3.result).to.equal({
+            provider: 'custom',
+            token: '456',
+            expiresIn: 3600,
+            refreshToken: undefined,
+            query: {},
+            profile: {
+                id: '1234567890',
+                displayName: 'Sample AD User',
+                email: 'sample@microsoft.com',
+                raw: profile
+            }
+        });
+    });
+
+    it('authenticates with mock azure AD and custom tenant', async (flags) => {
+
+        const mock = await Mock.v2(flags);
+        const server = Hapi.server({ host: 'localhost', port: 80 });
+        await server.register(Bell);
+
+        const custom = Bell.providers['azure-legacy']({ tenant: 'abc-def-ghi' });
+        Hoek.merge(custom, mock.provider);
+
+        const profile = {
+            oid: '1234567890',
+            name: 'Sample AD User',
+            upn: 'sample@microsoft.com'
+        };
+
+        Mock.override('https://login.microsoftonline.com/abc-def-ghi/openid/userinfo', profile);
+
+        server.auth.strategy('custom', 'bell', {
+            password: 'cookie_encryption_password_secure',
+            isSecure: false,
+            clientId: 'azuread',
+            clientSecret: 'secret',
+            provider: custom
+        });
+
+        server.route({
+            method: '*',
+            path: '/login',
+            config: {
+                auth: 'custom',
+                handler: function (request, h) {
+
+                    return request.auth.credentials;
+                }
+            }
+        });
+
+        const res1 = await server.inject('/login');
+        const cookie = res1.headers['set-cookie'][0].split(';')[0] + ';';
+
+        const res2 = await mock.server.inject(res1.headers.location);
+
+        const res3 = await server.inject({ url: res2.headers.location, headers: { cookie } });
+        expect(res3.result).to.equal({
+            provider: 'custom',
+            token: '456',
+            expiresIn: 3600,
+            refreshToken: undefined,
+            query: {},
+            profile: {
+                id: '1234567890',
+                displayName: 'Sample AD User',
+                email: 'sample@microsoft.com',
+                raw: profile
+            }
+        });
+    });
+});
