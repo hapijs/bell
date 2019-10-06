268826ce41fd801144ae6f32cde75a4705b7ca9c
hilanmiao <hilanmiao@126.com>
2019-10-06 10:57:06 +0800

    test coverage

--- test/providers/weixin.js
+++ test/providers/weixin.js
@@ -28,23 +28,19 @@ describe('weixin', () => {
         Hoek.merge(custom, mock.provider);
 
         const profile = {
-            openid: 'o96DVwVQ2VbKjgZMpnr30ogB_h8Q',
+            openid: '12345678',
             nickname: '张国栋',
             sex: 1,
             language: 'zh_CN',
             city: 'Weifang',
             province: 'Shandong',
             country: 'CN',
-            headimgurl: 'http://thirdwx.qlogo.cn/mmopen/vi_32/5cFyA1ZQGBCxOhFvUAudqOa481NkPIHVDdf7rLZOjIWJHPiabDbKia3CIyTmkThCWFeqAKBNrYOhT9ufM45Pw/132',
+            headimgurl: 'https://example.com/profile.png',
             privilege: [],
-            unionid: 'ou9a2v-kyFAt_3CZApMy0tWtv3BE'
+            unionid: '123456789'
         };
 
         Mock.override('https://api.weixin.qq.com/sns/userinfo', profile);
-
-        // https://open.weixin.qq.com/connect/qrconnect?appid=weixin&response_type=code&redirect_uri=http%3A%2F%2Flocalhost%2Flogin&state=7FRA7Pmis15gRaCjfLc761&scope=snsapi_login
-        // const  = {}
-        // Mock.override('https://open.weixin.qq.com/connect/qrconnect', profile);
 
         server.auth.strategy('custom', 'bell', {
             password: 'cookie_encryption_password_secure',
@@ -68,8 +64,8 @@ describe('weixin', () => {
         });
 
         const res1 = await server.inject('/login');
-
         const cookie = res1.headers['set-cookie'][0].split(';')[0] + ';';
+
         // const res2 = await mock.server.inject(res1.headers.location);
         const state = res1.headers.location.match(/state=(\S*)&scope/)[1];
         const location = 'http://localhost/login?code=1&state=' + state;
