7cf2519feb5a870bf68b029d0464449647843db2
Devin Ivy <devinivy@gmail.com>
2022-10-10 01:09:35 -0400

    Test ESM, fix location regression

---
+++ test/esm.js
@@ -0,0 +1,30 @@
+'use strict';
+
+const Code = require('@hapi/code');
+const Lab = require('@hapi/lab');
+
+
+const { before, describe, it } = exports.lab = Lab.script();
+const expect = Code.expect;
+
+
+describe('import()', () => {
+
+    let Bell;
+
+    before(async () => {
+
+        Bell = await import('../lib/index.js');
+    });
+
+    it('exposes all methods and classes as named imports', () => {
+
+        expect(Object.keys(Bell)).to.equal([
+            'default',
+            'oauth',
+            'plugin',
+            'providers',
+            'simulate'
+        ]);
+    });
+});
